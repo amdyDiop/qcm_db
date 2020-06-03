821498fb539e3f334ccb7877eca8846c9fa6770b
dofbi <amdymila@gmail,com>
2020-06-03 12:47:36 +0000

    navigation entre les page admin whith ajax

--- src/template/admin/admin.php
+++ src/template/admin/admin.php
@@ -48,13 +48,16 @@ $_SESSION['question'] =$db;
         <link rel="stylesheet" type="text/css" href="../../../assets/css/miniProjet.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!---------  library for icon     -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <div class="container justify-content-center mt-md-4 bg-primary ">
+        <div class="container justify-content-center mt-md-2 bg-primary ">
+            <!-- header-->
             <div class="row">
                 <div class="col-3">
                     <img src="../../../assets/Images/bg-01.jpg" class="rounded-circle " alt="" width="90" height="90">
@@ -70,86 +73,40 @@ $_SESSION['question'] =$db;
                 </div>
             </div>
         </div>
-        <div class="container justify-content-center mt-md-4 bg-primary ">
+        <!-- header-->
+
+        <!-- global content-->
+        <div class="container justify-content-center mt-md-2 bg-primary ">
+            <!-- navbar-->
             <div class="row">
+                <button type="button" id="listeJ" class=" btn btn-lg ml-5  mr-5 mt-2 text-white text-center h5 mauve" >Liste Joueurs </button>
+                <button type="button" id="newAdmin"  class="btn btn-lg ml-5 mt-2  mr-2 text-white text-center h5 mauve">Créer Admin</button>
+                <button type="button" id="listeQ"  class="btn btn-lg ml-5 mt-2  mr-4 text-white text-center h5 mauve">Liste Questions</button>
+                <button type="button" id="newQuestion"  class="btn btn-lg ml-5 mt-2 text-white text-center h5 mauve">Crée Question </button>
+            </div>
+            <!-- navbar-->
 
-                <div class="col-2 mt-ms-4  ml-5 mr-5 mb-4 pb-3 pt-2  text-white text-center h5 mauve">Liste Joueurs </div>
+            <!--  content -->
+            <div class="container bleu">
+                    <div id="container" class="row">
 
-                <div class="col-2 mt-ms-4  ml-5 mr-5 mb-4 pb-3 pt-2  text-white text-center h5 mauve">Créer Admin</div>
-
-                <div class="col-2 mt-ms-4  ml-5 mr-5 mb-4 pb-3 pt-2  text-white text-center h5 mauve">Liste Questions</div>
-                <div class="col-2 mt-ms-4  ml-5  mb-4 pb-3 pt-2  text-white text-center h5 mauve">Crée Question </div>
-
-
-                <div class="container bleu">
-                    <div class="row ">
-                        <div class="card col-10 bg-primary border-primary">
-                            <table class="table table-bordered table-hover text-center mt-2 ">
-                                <thead class="bleu">
-                                    <tr>
-                                        <th scope="col-3">Prénom</th>
-                                        <th scope="col">Nom</th>
-                                        <th scope="col">Sore</th>
-                                        <th scope="col">image</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="mauve">
-                                        <td>Mark</td>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr class="mauve">
-                                        <td>Jacob</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr class="mauve">
-                                        <td>Larry</td>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    <tr class="mauve">
-                                        <td>Larry</td>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    <tr class="mauve mb-4">
-                                        <td class="mb-4">Larry</td>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    <tr>
-                                        <tr class="mauve">
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
-                                            <td>@twitter</td>
-                                        </tr>
-                                </tbody>
-                            </table>
-
-                        </div>
-                        <div class="card col-2 bg-primary border-primary ">
-                            <div class="row justify-content-center pt-4 mb-5 mt-4 ">
-                                <input class="button mt-4  roundButton" type="submit" value=">">
-
-                            </div>
-                            <div class="row justify-content-center pt-4 mt-5">
-                                <button class="button mt-4 roundButton" type="button"><</button>
-
-                            </div>
-                        </div>
                     </div>
-
-
                 </div>
             </div>
+        <!--  content -->
+
+
+        <!-- Footer -->
+        <footer class="page-footer container mt-2 mb-1">
+            <div class="row noir">
+                <div class="col-4 ">
+                    <img src="../../../assets/Images/footer.PNG" class="rounded-circle " alt="" width="70" height="70">
+                </div>
+                <div class="footer-copyright text-center py-3 h3 text-white">© 2020 Copyright: amdy diop</div>
+            </div>
+            <!--a href="https://mdbootstrap.com/"> MDBootstrap.com</a-->
+        </footer>
+        <!-- Footer -->
             <script src=" ../../../assets/js/fonction.js "></script>
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js " integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN " crossorigin="anonymous "></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js " integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q " crossorigin="anonymous "></script>
@@ -157,6 +114,45 @@ $_SESSION['question'] =$db;
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js "></script>
             <script src="../../../assets/bower_components/jquery-validation/dist/jquery.validate.min.js "></script>
             <script src="../../../assets/js/fonction.js "></script>
+        <script>
+            $(document).ready(function(){
+                $("#listeJ").css("background-color", "#34BBE6");
+                $("#container").load("listeJoueur.php ");
+                    // un élément portant l'id "content" existe dans contenu.html
+                $("#listeJ").click(function () {
+                    $("#container").load("listeJoueur.php "); // un élément portant l'id "content" existe dans contenu.html
+                    $("#listeJ").css("background-color", "#34BBE6");
+                    $("#newAdmin").css("background-color", "#D96BCE");
+                    $("#listeQ").css("background-color", "#D96BCE");
+                    $("#newQuestion").css("background-color", "#D96BCE");
+
+                });
+                $("#listeQ").click(function () {
+                    $("#container").load("listeQuestion.php "); // un élément portant l'id "content" existe dans contenu.html
+                    $("#listeJ").css("background-color", "#D96BCE");
+                    $("#newAdmin").css("background-color", "#D96BCE");
+                    $("#listeQ").css("background-color", "#34BBE6");
+                    $("#newQuestion").css("background-color", "#D96BCE");
+                });
+                $("#newAdmin").click(function () {
+                    $("#container").load("newAdmin.php "); // un élément portant l'id "content" existe dans contenu.html
+                    $("#listeJ").css("background-color", "#D96BCE");
+                    $("#newAdmin").css("background-color", "#34BBE6");
+                    $("#listeQ").css("background-color", "#D96BCE");
+                    $("#newQuestion").css("background-color", "#D96BCE");
+
+                });
+                $("#newQuestion").click(function () {
+                    $("#container").load("newQuestion.php "); // un élément portant l'id "content" existe dans contenu.html
+                    $("#listeJ").css("background-color", "#D96BCE");
+                    $("#newAdmin").css("background-color", "#D96BCE");
+                    $("#listeQ").css("background-color", "#D96BCE");
+                    $("#newQuestion").css("background-color", "#34BBE6");
+
+                });
+
+            });
+        </script>
     </body>
 
     </html>
--- src/template/admin/listeJoueur.php
+++ src/template/admin/listeJoueur.php
@@ -1,31 +1,59 @@
-<div class="rightContent">
-    <div class="textListJoueur">
-        Liste des joueurs par score
+<div class="card col-10 bg-primary border-primary">
+    <table class="table table-bordered table-hover text-center mt-1 ">
+        <thead class="bleu">
+        <tr>
+            <th scope="col-3">Prénom</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Sore</th>
+            <th scope="col">image</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="mauve">
+            <td>Mark</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr class="mauve">
+            <td>Jacob</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr class="mauve">
+            <td>Larry</td>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        <tr class="mauve">
+            <td>Larry</td>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        <tr class="mauve mb-4">
+            <td class="mb-4">Larry</td>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        <tr>
+        <tr class="mauve">
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<div class="card col-2 bg-primary border-primary ">
+    <div class="row justify-content-center pt-4 mb-5 mt-4 ">
+        <input class="button mt-4  roundButton" type="submit" value=">">
     </div>
-    <div class="tabList">
-        <table>
-            <th> Nom</th>
-            <th>Prenom</th>
-            <th> score</th>
-            <?php
-            $taille = count($_SESSION['joueurs']);
-if (isset($_GET['liste'])){
-
-    if (  $_SESSION['page']<=ceil( $taille /14)-1)
-        $_SESSION['page']++;
-}
-
-            if (isset($_GET['listeprecedent'])){
-
-                if (  $_SESSION['page']>1)
-                    $_SESSION['page']--;
-            }
-
-            pagination($_SESSION['joueurs'],13,$_SESSION['page'],$taille);
-            ?>
-        </table>
+    <div class="row justify-content-center pt-4 mt-5">
+        <button class="button mt-4 roundButton" type="button"><</button>
     </div>
-     <a   class="suivant" href="admin.php?liste=<?=$_SESSION['page']?>">suivant</a>
-    <a   class="precedent" href="admin.php?listeprecedent=<?=$_SESSION['page']?>">précedant</a>
-
 </div>
